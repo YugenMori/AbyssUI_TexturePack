931086cf23899e180d590e53504b8aa92c6c999f
yugen <22729525+YugenMori@users.noreply.github>
2020-10-04 16:50:45 -0300

    1.0.1.2.rc2

--- AbyssUI_TexturePack.lua
+++ AbyssUI_TexturePack.lua
@@ -1,21 +1,23 @@
-local prefix = "AbyssUI_Version"
-local playerName = UnitName("player")
-local AddonVersion = GetAddOnMetadata("AbyssUI_TexturePack", "Version")
-
-local function OnEvent(self, event, ...)
-	if event == "CHAT_MSG_ADDON" then
-		--print(event, ...)
-	elseif event == "PLAYER_ENTERING_WORLD" then
-		local isInitialLogin, isReloadingUi = ...
-		if isInitialLogin or isReloadingUi then
-			C_ChatInfo.SendAddonMessage(prefix, AddonVersion, "WHISPER", playerName)
-			C_ChatInfo.RegisterAddonMessagePrefix(prefix)
-			C_ChatInfo.SendAddonMessage(prefix, AddonVersion, "WHISPER", playerName)
-		end
-	end
-end
-
 local f = CreateFrame("Frame")
 f:RegisterEvent("CHAT_MSG_ADDON")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:SetScript("OnEvent", OnEvent)
+
+AddonPrefix = "AbyssUI_Version";
+local AddonVersion = GetAddOnMetadata("AbyssUI_TexturePack", "Version")
+local function Addon_OnEvent(self, event, ...)
+	if event == "CHAT_MSG_ADDON" then
+		--print("AbyssUI_TexturePack Loaded")
+		return nil
+	elseif event == "PLAYER_LOGIN" then
+		local successfulRequest = C_ChatInfo.RegisterAddonMessagePrefix(AddonPrefix)
+		--print(successfulRequest);
+	elseif event == "PLAYER_ENTERING_WORLD" then
+		C_ChatInfo.SendAddonMessage(AddonPrefix, AddonVersion, "WHISPER", UnitName("player"))
+	end
+end
+local f = CreateFrame("Frame")
+f:SetScript("OnEvent", Addon_OnEvent)
+f:RegisterEvent("CHAT_MSG_ADDON");
+f:RegisterEvent("PLAYER_LOGIN");
+f:RegisterEvent("PLAYER_ENTERING_WORLD");
